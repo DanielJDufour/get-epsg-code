fc403c4d9e183f16b5edd6c678760ce89c829cd5
DanielJDufour <daniel.j.dufour@gmail.com>
2021-07-19 16:38:06 -0400

    replaced mocha, chai, and karma with flug

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const test = require('flug');
 const { readdirSync, readFileSync } = require('fs');
 
 const { UNSUPPORTED_MSG } = require('../globals.js');
@@ -8,55 +8,51 @@ const getEPSGCode = require(`../dist/${TEST_BUNDLE}`);
 
 const DEBUG = process.env.DEBUG || false;
 
-describe('Checking Other Files', function() {
-  it("Should identify MapServer mapfile with init", function() {
-    try {
-      const input = `PROJECTION\n   "init=epsg:26915"\nEND`;
-      const actualCode = getEPSGCode(input, { debug: DEBUG });
-      expect(actualCode).to.equal(26915);  
-    } catch (error) {
-      if (!TEST_BUNDLE.includes('mapfile')) {
-        console.error(error);
-        process.exit(1);
-      }
+test("Should identify MapServer mapfile with init", ({ eq }) => {
+  try {
+    const input = `PROJECTION\n   "init=epsg:26915"\nEND`;
+    const actualCode = getEPSGCode(input, { debug: DEBUG });
+    eq(actualCode, 26915);  
+  } catch (error) {
+    if (!TEST_BUNDLE.includes('mapfile')) {
+      console.error(error);
+      process.exit(1);
     }
-  });
+  }
 });
 
 
-describe('Checking Files Downloaded from epsg.io', function() {
-  it("Should identify all files correctly", function() {
-    const files = readdirSync('test/data').filter(fn => !fn.endsWith('.sh'));
-    console.log('files:', files);
-    files.forEach(filename => {
-      if (filename.startsWith('32149')) return;
-      const dataType = filename.split('.').pop();
-      const expectedCode = Number(filename.split('.')[0]);
-      const filepath = `test/data/${filename}`;
-      const text = readFileSync(filepath, 'utf-8');
-      if (text.trim() !== '') {
-        let actualCode;
-        try {
-          actualCode = getEPSGCode(text, { debug: DEBUG });
-          expect(actualCode).to.equal(expectedCode);
-        } catch (error) {
-          if (TEST_BUNDLE.includes(dataType.toLowerCase())) {
-            if (!error.message.toLowerCase() === UNSUPPORTED_MSG.replace('{}', dataType)) {
-              throw error;
-            }
-          } else {
-            console.log("================START ERROR LOGGING================");
-            console.log("TEST_BUNDLE:", [TEST_BUNDLE]);
-            console.log("dataType:", [dataType]);
-            console.log("error.message:", error);
-            console.log("expectedCode:", [expectedCode]);
-            console.error("actualCode:", [actualCode]);
-            console.log("================END ERROR LOGGING================");
-            process.exit(1);
-            //throw new Error(`Failed to get EPSG Code for ${filename}.`);
+test("Should identify all files correctly", ({ eq }) => {
+  const files = readdirSync('test/data').filter(fn => !fn.endsWith('.sh'));
+  console.log('files:', files);
+  files.forEach(filename => {
+    if (filename.startsWith('32149')) return;
+    const dataType = filename.split('.').pop();
+    const expectedCode = Number(filename.split('.')[0]);
+    const filepath = `test/data/${filename}`;
+    const text = readFileSync(filepath, 'utf-8');
+    if (text.trim() !== '') {
+      let actualCode;
+      try {
+        actualCode = getEPSGCode(text, { debug: DEBUG });
+        eq(actualCode, expectedCode);
+      } catch (error) {
+        if (TEST_BUNDLE.includes(dataType.toLowerCase())) {
+          if (!error.message.toLowerCase() === UNSUPPORTED_MSG.replace('{}', dataType)) {
+            throw error;
           }
+        } else {
+          console.log("================START ERROR LOGGING================");
+          console.log("TEST_BUNDLE:", [TEST_BUNDLE]);
+          console.log("dataType:", [dataType]);
+          console.log("error.message:", error);
+          console.log("expectedCode:", [expectedCode]);
+          console.error("actualCode:", [actualCode]);
+          console.log("================END ERROR LOGGING================");
+          process.exit(1);
+          //throw new Error(`Failed to get EPSG Code for ${filename}.`);
         }
       }
-    });
+    }
   });
 });
