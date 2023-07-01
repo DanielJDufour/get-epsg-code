a1d61f80cbf7e2cb33a2cd3ecb86d81609be8660
DanielJDufour <daniel.j.dufour@gmail.com>
2023-07-01 11:42:04 +0200

    added enums.js

---
+++ enums.js
@@ -0,0 +1,17 @@
+const FORMATS = {
+  ESRI_WKT: "esriwkt",
+  GEOSERVER: "geoserver",
+  MAPFILE: "mapfile",
+  MAPNIK: "mapnik",
+  OGC_GML: "gml",
+  OGC_XML: "xml",
+  OGC_WKT: "wkt",
+  POSTGIS: "postgis",
+  PROJ_4: "proj4",
+  PROJ_4_JS: "js",
+  PROJJSON: "PROJJSON"
+};
+
+module.exports = {
+  FORMATS
+};
